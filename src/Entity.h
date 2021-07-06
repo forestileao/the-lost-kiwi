20cdc9d876a38839f9849d08f4a16a6fd72dd9be
Pedro Leao <pedroforestileao@gmail.com>
2021-07-05 22:58:32 -0300

    Created Entity class and uml file base

--- src/Entity.h
+++ src/Entity.h
@@ -1,16 +1,15 @@
-//
-// Created by forestileao on 05/07/2021.
-//
-
 #ifndef ENTITY_H
 #define ENTITY_H
 
+#include <SFML/Graphics.hpp>
 
 class Entity
 {
 private:
+	sf::RectangleShape body;
 	int x;
 	int y;
+
 public:
 	Entity();
 	~Entity();
